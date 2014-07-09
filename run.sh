911cc7e8bef96fe1268f3066e7a6fcd6505a5c3c
Jay Anderson <jayanderson0@gmail.com>
2014-07-08 19:22:57 -0700

    Use release version (optimized).

--- run.sh
+++ run.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-#WIDTH=1024
-#HEIGHT=768
-WIDTH=512
-HEIGHT=512
+WIDTH=1024
+HEIGHT=768
 
 # Random noise:
 #ffmpeg random input: ffmpeg -y -f rawvideo -s ${WIDTH}x${HEIGHT} -pix_fmt rgb24 -r 24 -i /dev/urandom -an -vcodec mpeg4 random.mp4
@@ -13,4 +11,4 @@ HEIGHT=512
 
 # Play directly:
 #./target/turing | ffmpeg -y -f rawvideo -s ${WIDTH}x${HEIGHT} -pix_fmt rgb24 -i - -an -f mpeg2video - | vlc -
-./target/turing | vlc --demux rawvideo --rawvid-fps=25 --rawvid-width $WIDTH --rawvid-height $HEIGHT --rawvid-chroma RV24 -
+./target/release/turing | vlc --demux rawvideo --rawvid-fps=25 --rawvid-width $WIDTH --rawvid-height $HEIGHT --rawvid-chroma RV24 -
